17a942346a09f12a80f5d4e3a78a1f552ed1d4bb
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2019-10-26 02:29:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer((req, res) => {
   console.log(req.url)
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/plain','Access-Control-Allow-Origin': '*'});
   res.end('Hello World\n');
 }).listen(process.env.PORT, () => console.log('Server http://localhost:3000'));
