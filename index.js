01655b2f70f75c982b828d39c706fc9fdde37b4a
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2019-10-26 00:36:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ http.createServer((req, res) => {
   console.log(req.url)
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000, () => console.log('Server http://localhost:3000'));
+}).listen(process.env.PORT, () => console.log('Server http://localhost:3000'));
